7733bf0d6ab3f877aaca60f4448eb64b8c4af0b2
lihaodong <lihaodongmail@163.com>
2019-06-19 21:46:08 +0800

    移除多余组件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -16,33 +16,53 @@
       </div>
       <!--<el-card class="box-card">-->
       <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
+        <!--<span class="svg-container">-->
+        <!--<svg-icon icon-class="user" />-->
+        <!--</span>-->
         <el-input
           v-model="loginForm.username"
           :placeholder="$t('login.username')"
           name="username"
           type="text"
-          auto-complete="on"
         />
       </el-form-item>
 
       <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
+        <!--<span class="svg-container">-->
+        <!--<svg-icon icon-class="password" />-->
+        <!--</span>-->
         <el-input
           v-model="loginForm.password"
-          :type="passwordType"
           :placeholder="$t('login.password')"
           name="password"
-          auto-complete="on"
+          type="password"
           @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
+        >
+          <i slot="prefix" class="el-input__icon el-icon-search" />
+
+        </el-input>
+        <!--<span class="show-pwd" @click="showPwd">-->
+        <!--<svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" @keyup.enter.native="handleLogin"/>-->
+        <!--</span>-->
+      </el-form-item>
+      <el-form-item>
+        <el-col :span="12">
+          <el-form-item prop="captcha">
+            <el-input
+              v-model="loginForm.captcha"
+              type="test"
+              auto-complete="off"
+              placeholder="验证码, 单击图片刷新"
+              style="width: 100%;"
+            />
+          </el-form-item>
+        </el-col>
+        <el-col class="line" :span="1">&nbsp;</el-col>
+        <el-col :span="11">
+          <el-form-item>
+            <img style="width: 100%;" class="pointer" :src="loginForm.src" alt="" @click="refreshCaptcha">
+          </el-form-item>
+        </el-col>
       </el-form-item>
 
       <el-button
@@ -53,44 +73,19 @@
       >
         {{ $t('login.logIn') }}
       </el-button>
-
-      <div style="position:relative">
-        <div class="tips">
-          <span>{{ $t('login.username') }} : admin</span>
-          <span>{{ $t('login.password') }} : {{ $t('login.any') }}</span>
-        </div>
-        <div class="tips">
-          <span style="margin-right:18px;">
-            {{ $t('login.username') }} : editor
-          </span>
-          <span>{{ $t('login.password') }} : {{ $t('login.any') }}</span>
-        </div>
-
-        <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
-          {{ $t('login.thirdparty') }}
-        </el-button>
-      </div>
       <!--</el-card>-->
     </el-form>
-
-    <el-dialog :title="$t('login.thirdparty')" :visible.sync="showDialog">
-      {{ $t('login.thirdpartyTips') }}
-      <br>
-      <br>
-      <br>
-      <social-sign />
-    </el-dialog>
   </div>
 </template>
 
 <script>
 import { validUsername } from '@/utils/validate'
 import LangSelect from '@/components/LangSelect'
-import SocialSign from './socialsignin'
+import { formatData } from '@/utils/webUtils'
 
 export default {
   name: 'Login',
-  components: { LangSelect, SocialSign },
+  components: { LangSelect },
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
@@ -109,10 +104,13 @@ export default {
     return {
       loginForm: {
         username: 'admin',
-        password: 'admin'
+        password: 'admin',
+        captcha: '',
+        src: ''
+
       },
       loginRules: {
-        // username: [{ required: true, trigger: 'blur', validator: validateUsername }],
+        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
         password: [{ required: true, trigger: 'blur', validator: validatePassword }]
       },
       passwordType: 'password',
@@ -130,7 +128,7 @@ export default {
     }
   },
   created() {
-    // window.addEventListener('hashchange', this.afterQRScan)
+    this.refreshCaptcha()
   },
   destroyed() {
     // window.removeEventListener('hashchange', this.afterQRScan)
@@ -152,6 +150,7 @@ export default {
             this.$router.push({ path: this.redirect || '/' })
           }).catch(() => {
             this.loading = false
+            this.refreshCaptcha()
           })
         } else {
           console.log('error submit!!')
@@ -159,174 +158,40 @@ export default {
         }
       })
     },
-    afterQRScan() {
-      // const hash = window.location.hash.slice(1)
-      // const hashObj = getQueryObject(hash)
-      // const originUrl = window.location.origin
-      // history.replaceState({}, '', originUrl)
-      // const codeMap = {
-      //   wechat: 'code',
-      //   tencent: 'code'
-      // }
-      // const codeName = hashObj[codeMap[this.auth_type]]
-      // if (!codeName) {
-      //   alert('第三方登录失败')
-      // } else {
-      //   this.$store.dispatch('LoginByThirdparty', codeName).then(() => {
-      //     this.$router.push({ path: '/' })
-      //   })
-      // }
+    refreshCaptcha: function() {
+      this.loginForm.src = 'http://localhost:8081/captcha.jpg?t=' + new Date().getTime()
     }
   }
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
-  /* 修复input 背景不协调 和光标变色 */
-  /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
-  $bg: #283443;
-  $light_gray: #eee;
-  $cursor: #fff;
-
-  @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-    .login-container .el-input input {
-      color: $cursor;
-
-      &::first-line {
-        color: $light_gray;
-      }
-    }
-  }
-
-  /* reset element-ui css */
   .login-container {
-    .el-input {
-      display: inline-block;
-      height: 47px;
-      width: 85%;
-
-      input {
-        background: transparent;
-        border: 0px;
-        -webkit-appearance: none;
-        border-radius: 0px;
-        padding: 12px 5px 12px 15px;
-        color: $light_gray;
-        height: 47px;
-        caret-color: $cursor;
-
-        &:-webkit-autofill {
-          -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
-          -webkit-text-fill-color: $cursor !important;
-        }
-      }
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    background-clip: padding-box;
+    margin: 100px auto;
+    width:400px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+    .title {
+      margin: 0 auto 30px auto;
+      text-align: center;
+      color: #505458;
     }
-
+    .remember {
+      margin: 0 0 35px 0;
+    }
     .el-form-item {
       border: 1px solid rgba(255, 255, 255, 0.1);
-      background: rgba(0, 0, 0, 0.1);
       border-radius: 5px;
       color: #454545;
     }
   }
-</style>
-
-<style rel="stylesheet/scss" lang="scss" scoped>
-  $bg: #2d3a4b;
-  $dark_gray: #889aa4;
-  $light_gray: #eee;
-
   .login-container {
 
-      justify-content: center;
-      align-items: center;
-      height: 100%;
-      background-image:url(	https://aurora-1255840532.cos.ap-chengdu.myqcloud.com/1547428971990.jpg);
-      background-size: cover;
-
-    /*<!--min-height: 100%;-->*/
-    /*<!--width: 100%;-->*/
-    /*<!--background-color: $bg;-->*/
-    /*<!--overflow: hidden;-->*/
-
-    .login-form {
-      position: relative;
-      /*background: #ffffff;*/
-      width: 500px;
-      max-width: 100%;
-      padding: 160px 35px 0;
-      margin: 0 auto;
-      overflow: hidden;
-    }
-    /*.login-form {*/
-      /*border-radius: 6px;*/
-      /*background: #ffffff;*/
-      /*width: 365px;*/
-      /*padding: 25px 25px 5px 25px;*/
-      /*.el-input {*/
-        /*height: 38px;*/
-        /*input {*/
-          /*height: 38px;*/
-        /*}*/
-      /*}*/
-    /*}*/
-
-    .tips {
-      font-size: 14px;
-      color: #fff;
-      margin-bottom: 10px;
-
-      span {
-        &:first-of-type {
-          margin-right: 16px;
-        }
-      }
-    }
-
-    .svg-container {
-      padding: 6px 5px 6px 15px;
-      color: $dark_gray;
-      vertical-align: middle;
-      width: 30px;
-      display: inline-block;
-    }
-
-    .title-container {
-      position: relative;
-
-      .title {
-        font-size: 26px;
-        color: $light_gray;
-        margin: 0px auto 40px auto;
-        text-align: center;
-        font-weight: bold;
-      }
-
-      .set-language {
-        color: #fff;
-        position: absolute;
-        top: 3px;
-        font-size: 18px;
-        right: 0px;
-        cursor: pointer;
-      }
-    }
-
-    .show-pwd {
-      position: absolute;
-      right: 10px;
-      top: 7px;
-      font-size: 16px;
-      color: $dark_gray;
-      cursor: pointer;
-      user-select: none;
-    }
-
-    .thirdparty-button {
-      position: absolute;
-      right: 0;
-      bottom: 6px;
-    }
   }
 </style>
