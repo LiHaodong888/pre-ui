e1d8758b44151aab1d60c7a7ec2cd6fb5344cccf
lihaodong <lihaodongmail@163.com>
2019-06-21 14:28:46 +0800

    修改登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,80 +1,87 @@
 <template>
   <div class="login-container">
-    <el-form
-      ref="loginForm"
-      :model="loginForm"
-      :rules="loginRules"
-      class="login-form"
-      auto-complete="on"
-      label-position="left"
-    >
-      <div class="title-container">
-        <h3 class="title">
-          {{ $t('login.title') }}
-        </h3>
-        <lang-select class="set-language" />
-      </div>
-      <!--<el-card class="box-card">-->
-      <el-form-item prop="username">
-        <!--<span class="svg-container">-->
-        <!--<svg-icon icon-class="user" />-->
-        <!--</span>-->
-        <el-input
-          v-model="loginForm.username"
-          :placeholder="$t('login.username')"
-          name="username"
-          type="text"
-        />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <!--<span class="svg-container">-->
-        <!--<svg-icon icon-class="password" />-->
-        <!--</span>-->
-        <el-input
-          v-model="loginForm.password"
-          :placeholder="$t('login.password')"
-          name="password"
-          type="password"
-          @keyup.enter.native="handleLogin"
-        >
-          <i slot="prefix" class="el-input__icon el-icon-search" />
-
-        </el-input>
-        <!--<span class="show-pwd" @click="showPwd">-->
-        <!--<svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" @keyup.enter.native="handleLogin"/>-->
-        <!--</span>-->
-      </el-form-item>
-      <el-form-item>
-        <el-col :span="12">
-          <el-form-item prop="captcha">
-            <el-input
-              v-model="loginForm.captcha"
-              type="test"
-              auto-complete="off"
-              placeholder="验证码, 单击图片刷新"
-              style="width: 100%;"
-            />
-          </el-form-item>
-        </el-col>
-        <el-col class="line" :span="1">&nbsp;</el-col>
-        <el-col :span="11">
-          <el-form-item>
-            <img style="width: 100%;" class="pointer" :src="loginForm.src" alt="" @click="refreshCaptcha">
-          </el-form-item>
-        </el-col>
-      </el-form-item>
-
-      <el-button
-        :loading="loading"
-        type="primary"
-        style="width:100%;margin-bottom:30px;"
-        @click.native.prevent="handleLogin"
+    <div class="login-left">
+      <img src="/static/WechatIMG25.png" alt="" class="img">
+      <p class="title">Pre 权限管理快速开发框架</p>
+      <p>v 1.0</p>
+    </div>
+    <div class="login-right">
+      <el-form
+        ref="loginForm"
+        :model="loginForm"
+        :rules="loginRules"
+        class="login-form"
+        auto-complete="on"
+        label-position="left"
       >
-        {{ $t('login.logIn') }}
-      </el-button>
-      <!--</el-card>-->
-    </el-form>
+        <div class="title-container">
+          <lang-select style="float: right" class="set-language" />
+          <h3 class="title">
+            {{ $t('login.title') }}
+          </h3>
+        </div>
+        <!--<el-card class="box-card">-->
+        <el-form-item prop="username">
+          <!--<span class="svg-container">-->
+          <!--<svg-icon icon-class="user" />-->
+          <!--</span>-->
+          <el-input
+            v-model="loginForm.username"
+            :placeholder="$t('login.username')"
+            name="username"
+            type="text"
+          />
+        </el-form-item>
+
+        <el-form-item prop="password">
+          <!--<span class="svg-container">-->
+          <!--<svg-icon icon-class="password" />-->
+          <!--</span>-->
+          <el-input
+            v-model="loginForm.password"
+            :placeholder="$t('login.password')"
+            name="password"
+            type="password"
+            @keyup.enter.native="handleLogin"
+          >
+            <i slot="prefix" class="el-input__icon el-icon-search" />
+
+          </el-input>
+          <!--<span class="show-pwd" @click="showPwd">-->
+          <!--<svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" @keyup.enter.native="handleLogin"/>-->
+          <!--</span>-->
+        </el-form-item>
+        <el-form-item>
+          <el-col :span="12">
+            <el-form-item prop="captcha">
+              <el-input
+                v-model="loginForm.captcha"
+                type="test"
+                auto-complete="off"
+                placeholder="验证码, 单击图片刷新"
+                style="width: 100%;"
+              />
+            </el-form-item>
+          </el-col>
+          <el-col class="line" :span="1">&nbsp;</el-col>
+          <el-col :span="11">
+            <el-form-item>
+              <img style="width: 100%;" class="pointer" :src="loginForm.src" alt="" @click="refreshCaptcha">
+            </el-form-item>
+          </el-col>
+        </el-form-item>
+
+        <el-button
+          :loading="loading"
+          type="primary"
+          style="width:100%;"
+          @click.native.prevent="handleLogin"
+        >
+          {{ $t('login.logIn') }}
+        </el-button>
+        <!--</el-card>-->
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -158,7 +165,7 @@ export default {
       })
     },
     refreshCaptcha: function() {
-      this.loginForm.src = 'http://localhost:8081/captcha.jpg?t=' + new Date().getTime()
+      this.loginForm.src = 'https://api.52lhd.com/captcha.jpg?t=' + new Date().getTime()
     }
   }
 }
@@ -166,31 +173,43 @@ export default {
 
 <style rel="stylesheet/scss" lang="scss">
   .login-container {
-    -webkit-border-radius: 5px;
+    width:1000px;
+    display: flex;
     border-radius: 5px;
-    -moz-border-radius: 5px;
-    background-clip: padding-box;
-    margin: 100px auto;
-    width:400px;
-    padding: 35px 35px 15px 35px;
+    overflow: hidden;
     background: #fff;
-    border: 1px solid #eaeaea;
     box-shadow: 0 0 25px #cac6c6;
-    .title {
-      margin: 0 auto 30px auto;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    padding: 40px 0;
+    .login-left{
+      width: 50%;
+      /*background: #1F79D6;*/
       text-align: center;
-      color: #505458;
+      padding-top: 60px;
+      border-right: 1px solid #00000055;
+      img{
+        width: 140px;
+      }
     }
-    .remember {
-      margin: 0 0 35px 0;
+    .login-right{
+      width: 50%;
+      padding: 35px;
+      .title {
+        margin: 0 auto 30px auto;
+        text-align: center;
+        color: #505458;
+      }
+      .remember {
+        margin: 0 0 35px 0;
+      }
+      .el-form-item {
+        border: 1px solid rgba(255, 255, 255, 0.1);
+        border-radius: 5px;
+        color: #454545;
+      }
     }
-    .el-form-item {
-      border: 1px solid rgba(255, 255, 255, 0.1);
-      border-radius: 5px;
-      color: #454545;
-    }
-  }
-  .login-container {
-
   }
 </style>
